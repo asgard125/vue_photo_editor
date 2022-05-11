586ae0fb831b3085bcaea37815479829032b62fd
asgard125 <asgardkarpov@yandex.ru>
2022-05-11 18:33:21 +1000

    added paintbucked, switch tools and color sucker, changed css styles

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app" class="main-div" @mouseleave="stopDrawing" @mouseup.left="stopDrawing">
     <div style="display: flex; flex-direction: row; align-items: center">
-      <button class="btn btn-primary" id="uploadbtn" @click="upload">Upload</button>
-      <button class="btn btn-primary" id="savebtn" @click="download">Download</button>
+      <button class="btn btn-primary" id="uploadbtn" @click="upload">Загрузить</button>
+      <button class="btn btn-primary" id="savebtn" @click="download">Скачать</button>
     <ImgEditFuncs @resize-canvas="resize" @apply-filters="filters" @apply-brightness-contrast="brightness_contrast"
                   @flip-horizontal="flip_horizontal" @flip-vertical="flip_vertical" @rotate="rotate_90"
     />
@@ -10,10 +10,24 @@
     <div class="painter-div">
       <div style="margin-right: 1%">
       <Chrome v-model="colors"/>
+        <div style="display: flex; flex-direction: column;">
       <div style="margin-top: 3%; display: flex; flex-direction: row;">
-        <button class="btn btn-light" @click="MinusWidth">-</button>
-        <input class="form-control" style="width: 70px; margin: 1%" type="number" v-model="lineWidth">
-        <button class="btn btn-light" @click="PlusWidth">+</button></div>
+        <button class="btn btn-light" @click="MinusWidth" style="margin-right: 1%; background: #E0E0E0;">-</button>
+        <input class="form-control" style="width: 70px;" type="number" v-model="lineWidth">
+        <button class="btn btn-light" @click="PlusWidth" style="margin-left: 1%; background: #E0E0E0;">+</button>
+      </div>
+          Тип линии
+          <div id="line-types" style="margin-top: 3%; display: flex; flex-direction: row;">
+          <button class="btn btn-light" value="round" @click="setTool" style="margin-right: 1%; background: #AAAAAA;">●</button>
+            <button class="btn btn-light" value="butt" @click="setTool" style="margin-right: 1%; background: #E0E0E0">/</button>
+            <button class="btn btn-light" value="square" @click="setTool" style="margin-right: 1%; background: #E0E0E0">■</button>
+          </div>
+          Инструменты
+          <div id="tools" style="margin-top: 3%; display: flex; flex-direction: row;">
+            <button class="btn btn-light" value="filler" @click="setTool" style="margin-right: 1%; background: #E0E0E0">Заливка</button>
+            <button class="btn btn-light" value="colorSucker" @click="setTool" style="margin-right: 1%; background: #E0E0E0">Пипетка</button>
+          </div>
+      </div>
       </div>
       <canvas id="paint_field" class="canvas" width=400 height=300 @mousemove="draw" @mousedown.left="beginDrawing" @mouseleave="ResetCords" v-on:keyup="return_back"/>
     </div>
@@ -40,12 +54,15 @@ export default {
       y: 0,
       lineWidth: 5,
       scale: 1,
+      tool: 'round',
+      activeToolColor: '#AAAAAA',
+      inactiveToolColor: '#E0E0E0',
       isDrawing: false,
       colors: {rgba: {r: 0, g: 0, b: 0, a: 1}}
     }
   },
   mounted() {
-    var c = document.getElementById("paint_field");
+    let c = document.getElementById("paint_field");
     this.canvas = c.getContext('2d');
     this.canvas.imageSmoothingQuality = 'high';
     this.canvas.imageSmoothingEnabled = true;
@@ -69,7 +86,7 @@ export default {
       return tempCanvas;
     },
     make_history(){
-      if(this.history.length < 5){
+      if(this.history.length === 5){
         this.history.shift();
       }
       let tempCanvas = this.getTempCanvas();
@@ -154,11 +171,28 @@ export default {
       const {rgba: {r, g, b, a}} = this.colors;
       this.canvas.strokeStyle = `rgba(${r}, ${g}, ${b}, ${a})`;
       this.canvas.lineWidth = this.lineWidth;
-      this.canvas.lineCap = 'round';
+      this.canvas.lineCap = this.tool;
       this.canvas.moveTo(x1, y1);
       this.canvas.lineTo(x2, y2);
       this.canvas.stroke();
       this.canvas.closePath();
+    },
+    resetTools(){
+      let tools = document.getElementById("tools");
+      for (let i = 0; i < tools.children.length; i++) {
+        let elem = tools.children[i];
+        elem.style.background = this.inactiveToolColor;
+      }
+      let lines = document.getElementById("line-types");
+      for (let i = 0; i < lines.children.length; i++) {
+        let elem = lines.children[i];
+        elem.style.background = this.inactiveToolColor;
+      }
+    },
+    setTool(e){
+      this.resetTools();
+      this.tool = e.target.value;
+      e.target.style.background = this.activeToolColor;
     },
     draw(e) {
       if (this.isDrawing) {
@@ -172,12 +206,81 @@ export default {
         this.y = e.offsetY;
       }
     },
+    fill_canvas(startX, startY){
+      let img = this.canvas.getImageData(0, 0, this.canvas.canvas.width, this.canvas.canvas.height);
+      let imgData = img.data;
+      const {rgba: {r, g, b}} = this.colors;
+      let canvasWidth = this.canvas.canvas.width;
+      let canvasHeight = this.canvas.canvas.height;
+      let r_target = imgData[startY * canvasWidth * 4 + startX * 4];
+      let g_target = imgData[startY * canvasWidth * 4 + startX * 4 + 1];
+      let b_target = imgData[startY * canvasWidth * 4 + startX * 4 + 2];
+      if (startX > 0 && startX < this.canvas.canvas.width && startY > 0 && startY < this.canvas.canvas.height) {
+        let pixelStack = [[startX, startY]];
+        let newPos, x, y, pixelPos;
+        let colored = Array(canvasWidth * canvasHeight * 4).fill(0);
+        while (pixelStack.length > 0) {
+          newPos = pixelStack.pop();
+          x = newPos[0];
+          y = newPos[1];
+          pixelPos = (y * canvasWidth + x) * 4;
+          while (y-- >= 0 && this.matchStartColor(pixelPos, imgData, r_target, g_target, b_target) && colored[pixelPos] !== 1) {
+            pixelPos -= canvasWidth * 4;
+          }
+          pixelPos += canvasWidth * 4;
+          ++y;
+          while (y++ < canvasHeight - 1 && this.matchStartColor(pixelPos, imgData, r_target, g_target, b_target) && colored[pixelPos] !== 1) {
+            this.colorPixel(pixelPos, imgData, r, g ,b);
+            colored[pixelPos] = 1;
+            if (x > 0) {
+              if (this.matchStartColor(pixelPos - 4, imgData, r_target, g_target, b_target) && colored[pixelPos - 4] !== 1) {
+                  pixelStack.push([x - 1, y]);
+              }
+            }
+
+            if (x < canvasWidth - 1) {
+              if (this.matchStartColor(pixelPos + 4, imgData, r_target, g_target, b_target) && colored[pixelPos + 4] !== 1) {
+                  pixelStack.push([x + 1, y]);
+              }
+            }
+
+            pixelPos += canvasWidth * 4;
+          }
+        }
+        this.canvas.putImageData(img, 0, 0);
+      }
+    },
+    matchStartColor(pixelPos, imgData, r_target, g_target, b_target) {
+      let r_current = imgData[pixelPos];
+      let g_current = imgData[pixelPos + 1];
+      let b_current = imgData[pixelPos + 2];
+
+      return (r_current === r_target && g_current === g_target && b_current === b_target);
+    },
+    colorPixel(pixelPos, imgData, r, g ,b) {
+      imgData[pixelPos] = r;
+      imgData[pixelPos + 1] = g;
+      imgData[pixelPos + 2] = b;
+      imgData[pixelPos + 3] = 255;
+    },
+    pixelAt(x, y) {
+      return this.canvas.getImageData(x, y, 1, 1).data;
+    },
     beginDrawing(e) {
-      this.make_history();
-      this.x = e.offsetX;
-      this.y = e.offsetY;
-      this.drawLine(this.x , this.y, this.x , this.y)
-      this.isDrawing = true;
+      if(this.tool === 'colorSucker'){
+        let color = this.pixelAt(e.offsetX, e.offsetY);
+        this.colors = {rgba: {r: color[0], g: color[1], b: color[2], a: color[3]}}
+      }
+      else if (this.tool === 'filler') {
+        this.make_history();
+        this.fill_canvas(e.offsetX, e.offsetY);
+      }else {
+        this.make_history();
+        this.x = e.offsetX;
+        this.y = e.offsetY;
+        this.drawLine(this.x, this.y, this.x, this.y)
+        this.isDrawing = true;
+      }
     },
     stopDrawing() {
       this.isDrawing = false;
--- src/components/ImgEditFuncs.vue
+++ src/components/ImgEditFuncs.vue
@@ -2,7 +2,7 @@
   <div style="display: flex; flex-direction: row; align-items: center">
   <div class="dropdown">
     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-      Change size
+      Изменить размер
     </button>
     <div class="dropdown-menu" style="margin: 2%; min-width: 200px" aria-labelledby="dropdownMenuButton1">
       <div style="margin-left: 5%; display: flex; flex-direction: row; align-items: center">
@@ -11,54 +11,54 @@
       </div>
       <div style="display: flex; flex-direction: row; align-items: center; justify-content: center">
         <input type="radio" id="scale" value="scale" v-model="resizeType">
-        <label for="scale" style="margin-right: 2%">scale</label>
+        <label for="scale" style="margin-right: 2%">растянуть</label>
         <input type="radio" id="noscale" value="noscale" v-model="resizeType">
-        <label for="noscale">noscale</label>
+        <label for="noscale">расширить</label>
       </div>
-      <center><button class="btn btn-primary" id="reszie" @click="$emit('resize-canvas', resizeType, width, height);">Resize</button></center>
+      <center><button class="btn btn-primary" id="reszie" @click="$emit('resize-canvas', resizeType, width, height);">Применить</button></center>
     </div>
   </div>
     <div class="dropdown">
       <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-        Brightness&contrast
+        Яркость и контраст
       </button>
       <div class="dropdown-menu" style="margin: 2%; min-width: 200px" aria-labelledby="dropdownMenuButton1">
         <div style="display: flex; flex-direction: column; align-items: center; justify-content: space-around">
-        <center>Brightness</center>
+        <center>Яркость</center>
         <input type="range" v-model="brightness" min="50" max="150">{{ brightness }}%
-        <center>Contrast</center>
+        <center>Контраст</center>
         <input type="range" v-model="contrast" min="50" max="150">{{ contrast }}%
-        <center>Saturation</center>
+        <center>Насыщенность</center>
         <input type="range" v-model="saturate" min="50" max="150">{{ saturate }}%
-        <button class="btn btn-primary" id="apply_brightness_contrast" @click=send_brightness_contrast_data>Apply</button>
+        <button class="btn btn-primary" id="apply_brightness_contrast" @click=send_brightness_contrast_data>Применить</button>
         </div>
       </div>
     </div>
     <div class="dropdown">
       <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton3" data-bs-toggle="dropdown" aria-expanded="false">
-        Filters
+        Фильтры
       </button>
       <div class="dropdown-menu" style="margin: 2%; min-width: 200px" aria-labelledby="dropdownMenuButton2">
         <div style="display: flex; flex-direction: column; align-items: center; justify-content: space-around">
-          <center>Negative</center>
+          <center>Негатив</center>
           <input type="range" v-model="invert" min="0" max="100">{{ invert }}%
-          <center>Sepia</center>
+          <center>Сепия</center>
           <input type="range" v-model="sepia" min="0" max="100">{{ sepia }}%
-          <center>Grayscale</center>
+          <center>Оттенки серого</center>
           <input type="range" v-model="grayscale" min="0" max="100">{{ grayscale }}%
-          <button class="btn btn-primary" id="apply_filters" @click=send_filters_data>Apply</button>
+          <button class="btn btn-primary" id="apply_filters" @click=send_filters_data>Применить</button>
         </div>
       </div>
     </div>
     <div class="dropdown">
       <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton4" data-bs-toggle="dropdown" aria-expanded="false">
-        Direction
+        Положение
       </button>
       <div class="dropdown-menu" style="margin: 2%; min-width: 200px" aria-labelledby="dropdownMenuButton2">
         <div style="display: flex; flex-direction: column; align-items: center; justify-content: space-around">
-          <button class="btn btn-primary" style="margin: 1%; width: 130px" id="rotate" @click="$emit('rotate')">rotate 90</button>
-          <button class="btn btn-primary" style="margin: 1%; width: 130px" id="flip-hor" @click="$emit('flip-horizontal')">flip horizontal</button>
-          <button class="btn btn-primary" style="margin: 1%; width: 130px" id="flip-vert" @click="$emit('flip-vertical')">flip vertical</button>
+          <button class="btn btn-primary" style="margin: 1%; width: 130px" id="rotate" @click="$emit('rotate')">поворот на 90</button>
+          <button class="btn btn-primary" style="margin: 1%; width: 130px" id="flip-hor" @click="$emit('flip-horizontal')">отразить горизонтально</button>
+          <button class="btn btn-primary" style="margin: 1%; width: 130px" id="flip-vert" @click="$emit('flip-vertical')">отразить вертикально</button>
         </div>
       </div>
     </div>
@@ -102,5 +102,10 @@ export default {
 </script>
 
 <style scoped>
+
+.btn-secondary{
+  background-color: #9B9B9C;
+  border-color: #9B9B9C;
+}
 
 </style>
